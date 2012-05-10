a6f2539dc572bc5fcf35ba090139224d01021df7
Jack Callister <jarsbe@gmail.com>
2012-05-10 17:01:20 +1200

    Generate 4 Homestay's.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,18 @@
 if Rails.env.development? || Rails.env.staging?
   Homestay.destroy_all
-  Homestay.create({
-    title:        "My First Homestay",
-    location:     "Wellington",
-    price:        15,
-    description:  <<-eos
-      Maecenas faucibus mollis interdum. Lorem ipsum dolor sit amet, consectetur
-      adipiscing elit. Nulla vitae elit libero, a pharetra augue. Cras mattis
-      consectetur purus sit amet fermentum. Cum sociis natoque penatibus et
-      magnis dis parturient montes, nascetur ridiculus mus.
-    eos
-  })
+  4.times do
+    Homestay.create({
+      title:        "My First Homestay",
+      location:     "Wellington",
+      price:        15,
+      description:  <<-eos
+        Maecenas faucibus mollis interdum. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Nulla vitae elit libero, a pharetra augue. Cras mattis
+        consectetur purus sit amet fermentum. Cum sociis natoque penatibus et
+        magnis dis parturient montes, nascetur ridiculus mus.
+      eos
+    })
+  end
   User.destroy_all
   User.create({
     email:                  "test@example.com",
