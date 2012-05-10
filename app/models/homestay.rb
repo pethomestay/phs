81c84f56ce7e16c2087e19e5ceac9cca62d84a0e
Jack Callister <jarsbe@gmail.com>
2012-05-10 13:18:20 +1200

    Build basic Hotel layout.

--- app/models/homestay.rb
+++ app/models/homestay.rb
@@ -5,10 +5,10 @@ class Homestay
 
 	def initialize(id)
 		@id = id
-		@title = "Example"
+		@title = "Example of a longer title"
 		@price = 15.0
-		@location = "Welly"
-		@description = "Maecenas faucibus mollis interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Cras mattis consectetur purus sit amet fermentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+		@location = "Te Aro, Wellington"
+		@description = "Maecenas faucibus mollis interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla vitae elit libero, a pharetra augue. Cras mattis consectetur purus sit amet fermentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere consectetur est at lobortis. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit."
 	end
 
 	attr_reader :id, :title, :price, :location, :description
