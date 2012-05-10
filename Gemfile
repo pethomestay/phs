e7be34786d987866759bcb098e3bc06fa6afca50
Sebastian Edwards <s.thursday@gmail.com>
2012-05-10 14:45:40 +1200

    Installing devise gem.

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.2.3'
 gem 'jquery-rails'
 gem 'haml-rails'
 gem 'unicorn'
+gem 'devise'
 
 group :development do
   gem 'sqlite3'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,9 @@ PetHomestay::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
+  # Add default mailer URL
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
