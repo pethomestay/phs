a524b5de2da0b4488e061dd74c20e96f9c99f578
Jack Callister <jarsbe@gmail.com>
2012-05-14 16:29:43 +1200

    Naming routes.

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,13 @@ PetHomestay::Application.routes.draw do
 
   root to: "pages#home"
 
-  match '/how-it-works',  to: 'pages#how_it_works'
-  match '/contact', to: 'pages#contact'
-  match '/terms-and-conditions',  to: 'pages#terms_and_conditions'
-  match '/privacy-policy',  to: 'pages#privacy_policy'
+  get '/how-it-works',          to: 'pages#how_it_works', as: 'how_it_works'
   get '/why-pet-homestay',      to: 'pages#why_pet_homestay', 
                                 as: 'why_pet_homestay'
+  get '/contact',               to: 'pages#contact'
+  get '/terms-and-conditions',  to: 'pages#terms_and_conditions',
+                                as: 'terms_and_conditions'
+  get '/privacy-policy',        to: 'pages#privacy_policy',
+                                as: 'privacy_policy'
   
 end
