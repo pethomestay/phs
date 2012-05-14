a00f3ccb4d07f26d0667ca5e010cf2878e6c9c32
Sebastian Edwards <s.thursday@gmail.com>
2012-05-14 16:04:59 +1200

    Adding star rating helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,16 @@ module ApplicationHelper
         type.to_s
     end
   end
+
+  def rating_stars(rating)
+    haml_tag :p do
+      5.times.collect do |star|
+        if star < rating
+          haml_tag :i, class: 'icon-star'
+        else
+          haml_tag :i, class: 'icon-star-empty'
+        end
+      end
+    end
+  end
 end
