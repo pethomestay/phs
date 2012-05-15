84301a3175dfa183abd69f3cd5dde8fa7dd2b5db
Jack Callister <jarsbe@gmail.com>
2012-05-15 14:24:50 +1200

    Adding hotel seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,19 @@
 if Rails.env.development? || Rails.env.staging?
-  Sitter.destroy_all
-  4.times do
-    Sitter.create({
-      title:        "My First Homestay",
-      location:     "Wellington",
-      price:        15,
-      description:  <<-eos
-        Maecenas faucibus mollis interdum. Lorem ipsum dolor sit amet, consectetur
-        adipiscing elit. Nulla vitae elit libero, a pharetra augue. Cras mattis
-        consectetur purus sit amet fermentum. Cum sociis natoque penatibus et
-        magnis dis parturient montes, nascetur ridiculus mus.
-      eos
-    })
+  [Sitter, Hotel].collect do |klass|
+    klass.destroy_all
+    4.times do
+      klass.create({
+        title:        "My First Homestay",
+        location:     "Wellington",
+        price:        15,
+        description:  <<-eos
+          Maecenas faucibus mollis interdum. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Nulla vitae elit libero, a pharetra augue. Cras mattis
+          consectetur purus sit amet fermentum. Cum sociis natoque penatibus et
+          magnis dis parturient montes, nascetur ridiculus mus.
+        eos
+      })
+    end
   end
   User.destroy_all
   User.create({
