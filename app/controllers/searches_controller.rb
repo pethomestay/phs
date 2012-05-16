8140ef7fffc1666e5e5fde85e543537d809ff951
Sebastian Edwards <s.thursday@gmail.com>
2012-05-17 11:51:32 +1200

    Use form_for for search refinement box.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,8 +1,8 @@
 class SearchesController < ApplicationController
   def create
-    search = Search.new params[:search]
-    if search.valid?
-    	@providers = search.provider_class.all
+    @search = Search.new params[:search]
+    if @search.valid?
+    	@providers = @search.provider_class.all
     else
       raise "Invalid search"
     end
