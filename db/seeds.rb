0521a2ac242b9f19c4bb314e2f53150b14f619a5
Sebastian Edwards <s.thursday@gmail.com>
2012-05-28 15:26:22 +1200

    Updating seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
 if Rails.env.development? || Rails.env.staging?
+  User.destroy_all
+  User.create({
+    email:                  "test@example.com",
+    password:               "test2010",
+    password_confirmation:  "test2010"
+  })
   Sitter.destroy_all
   4.times do
-    Sitter.create({
+    user = User.create({
+      email:                  "#{SecureRandom.hex}@pethomestay.com",
+      password:               "test2010",
+      password_confirmation:  "test2010",
+      address_1:              "94 Dixon Street",
+      address_suburb:         "Te Aro",
+      address_city:           "Wellington"
+    })
+    user.build_sitter({
       title:        "My First Homestay",
-      location:     "Wellington",
       price:        15,
       description:  <<-eos
         Maecenas faucibus mollis interdum. Lorem ipsum dolor sit amet, consectetur
@@ -11,16 +24,15 @@ if Rails.env.development? || Rails.env.staging?
         consectetur purus sit amet fermentum. Cum sociis natoque penatibus et
         magnis dis parturient montes, nascetur ridiculus mus.
       eos
-    })
+    }).save
   end
   Hotel.destroy_all
-  4.times do
+  50.times do
     Hotel.create({
       title:          "A Pet Hotel",
       address_1:      "94 Dixon Street",
       address_suburb: "Te Aro",
       address_city:   "Wellington",
-      location:       "Wellington",
       price:          15,
       description:    <<-eos
         Maecenas faucibus mollis interdum. Lorem ipsum dolor sit amet, consectetur
@@ -30,10 +42,4 @@ if Rails.env.development? || Rails.env.staging?
       eos
     })
   end
-  User.destroy_all
-  User.create({
-    email:                  "test@example.com",
-    password:               "test2010",
-    password_confirmation:  "test2010"
-  })
 end
