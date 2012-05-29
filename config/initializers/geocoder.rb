b45ff3d1cc00b7714ec06e9c757ac99f28b0713e
Sebastian Edwards <s.thursday@gmail.com>
2012-05-29 14:22:53 +1200

    Setting default geocoder units to km.

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -2,4 +2,5 @@ Geocoder.configure do |config|
   config.lookup = :bing
   config.api_key = 'AvPoJ5-fGVx4lv7m9JaDGmiX213KBlrAh373TEk_5epnjPgK79vGD1o58voxGIJw'
   config.timeout = 6
+  config.units = :km
 end
