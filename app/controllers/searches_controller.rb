c5e7351c7da8c0dac04f47d33f32f88039337da8
Sebastian Edwards <s.thursday@gmail.com>
2012-06-21 11:24:35 +1200

    Fixing error where empty search results would crash.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -15,11 +15,11 @@ class SearchesController < ApplicationController
       	provider = klass.near([@search.latitude, @search.longitude], within, order: 'distance')
         providers = providers << provider.to_a if provider.present?
       end
-      providers.sort! {|a,b| a.length <=> b.length}
-      longest = providers.pop
-      @providers = longest.zip(*providers).flatten
-      @providers.reject!(&:nil?)
-      if @providers.present?
+      if providers.present?
+        providers.sort! {|a,b| a.length <=> b.length}
+        longest = providers.pop
+        @providers = longest.zip(*providers).flatten
+        @providers.reject!(&:nil?)
         @providers = @providers.paginate(page: params[:page], per_page: 10)
       else
         redirect_to no_results_path(location: @search.location, provider_type: @search.provider_types)
