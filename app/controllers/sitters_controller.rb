4b1c10d71925162b215bc659e4ae815f00497789
Sebastian Edwards <s.thursday@gmail.com>
2012-06-22 13:12:21 +1200

    Updating sitters view action to mirror hotel.

--- app/controllers/sitters_controller.rb
+++ app/controllers/sitters_controller.rb
@@ -2,5 +2,9 @@ class SittersController < ApplicationController
   def show
     @sitter = Sitter.find(params[:id])
     @title = @sitter.title
+    @reviewed_ratings = @sitter.ratings.reviewed
+    if current_user
+      @my_rating = Rating.find_by_user_id_and_ratable_type_and_ratable_id(current_user.id, 'Sitter', params[:id])
+    end
   end
 end
