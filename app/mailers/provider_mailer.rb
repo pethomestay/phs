586ab4692d7ae3e1c5135efceedb76c17c009282
Sebastian Edwards <s.thursday@gmail.com>
2012-06-28 13:44:04 +1200

    Send email to provider on enquiry creation.

---
+++ app/mailers/provider_mailer.rb
@@ -0,0 +1,12 @@
+class ProviderMailer < ActionMailer::Base
+  default from: "Pet Homestay <admin@pethomestay.com>"
+
+  def enquiry_email(enquiry)
+    @enquiry = enquiry
+    @user = @enquiry.user
+    @pets = @user.pets
+    provider_user = enquiry.provider.user
+    email_with_name = "#{provider_user.first_name} #{provider_user.last_name} <#{provider_user.email}>"
+    mail(to: email_with_name, subject: "You've recieved an enquiry")
+  end
+end
