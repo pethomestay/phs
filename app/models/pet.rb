7feed68f2388e6b00bd45d92dd7135307830f8af
Sebastian Edwards <s.thursday@gmail.com>
2012-07-24 09:31:32 +1200

    Adding field to specify other pet type.

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -7,10 +7,13 @@ class Pet < ActiveRecord::Base
   attr_accessible :breed, :name, :age, :pet_type, :size, :sex, :microchip_number, \
                   :council_number, :dislike_people, :dislike_animals, \
                   :dislike_children, :dislike_loneliness, :explain_dislikes, \
-                  :pictures, :pictures_attributes, :flea_treated, :vaccinated, :house_trained
-  attr_accessor :sex, :age, :microchip_number, :council_number, :size, :explain_dislikes
+                  :pictures, :pictures_attributes, :flea_treated, :vaccinated, :house_trained, \
+                  :other_pet_type
+  attr_accessor :sex, :age, :microchip_number, :council_number, :size, :explain_dislikes, :other_pet_type
 
   validates_presence_of :name, :age
   validates_inclusion_of :pet_type, :in => %w( dog cat bird fish other )
   # validates_presence_of :microchip_number, if: proc {|pet| pet.type == 'dog'}
+  
+  validates_presence_of :other_pet_type, if: proc {|pet| pet.pet_type == 'other'}
 end
