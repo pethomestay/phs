af1f4ab0d86fb32beb8ce605f189d6b1d2b7ee49
Sebastian Edwards <s.thursday@gmail.com>
2012-07-25 16:29:04 +1200

    Fixing ratings.

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -2,7 +2,7 @@ class RatingsController < ApplicationController
   def update
     if current_user
       klass = env['PATH_INFO'].split('/').second.singularize.capitalize
-      @rating = Rating.find_or_create_by_user_id_and_ratable_type_and_ratable_id(current_user.id, klass, params[:hotel_id])
+      @rating = Rating.find_or_create_by_user_id_and_ratable_type_and_ratable_id(current_user.id, klass, params[:homestay_id])
       @rating.update_attributes(params[:rating])
       render json: @rating.to_json
     else
