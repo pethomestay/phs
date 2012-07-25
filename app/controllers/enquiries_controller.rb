2534120976bbc3dc3385f9d40d863103ba800b9c
Sebastian Edwards <s.thursday@gmail.com>
2012-07-25 14:46:32 +1200

    Updating enquiry flash message.

--- app/controllers/enquiries_controller.rb
+++ app/controllers/enquiries_controller.rb
@@ -3,7 +3,7 @@ class EnquiriesController < ApplicationController
     @enquiry = Enquiry.new(params[:enquiry])
     @enquiry.user = current_user
     if @enquiry.save && ProviderMailer.enquiry_email(@enquiry).deliver
-      redirect_to @enquiry.homestay, alert: "Your enquiry has been sent to this provider."
+      redirect_to @enquiry.homestay, alert: "Your enquiry has been sent to this provider. They will respond with their availability soon."
     else
       redirect_to @enquiry.homestay, error: "There was an issue with your request. Please contact support."
     end
