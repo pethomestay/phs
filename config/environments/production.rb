567feaf85e8f4b5a174fd41fbca90b4eba5d8d3c
Sebastian Edwards <s.thursday@gmail.com>
2012-08-20 13:14:05 +1200

    Use mailgun in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,17 @@ PetHomestay::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  # Use mailgun for ActionMailer
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'], 
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => ENV['HOST'],
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
+
   # Add default mailer URL
   config.action_mailer.default_url_options = { :host => ENV['HOST'] }
   config.action_mailer.asset_host = "http://#{ENV['HOST']}"
