dc6ae6a74ed580161544f1ef7191bf91e59dd6fa
Sebastian Edwards <s.thursday@gmail.com>
2012-09-03 11:31:29 +1200

    Reverting changes.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -37,9 +37,7 @@ class SearchesController < ApplicationController
 
   def perfrom_geocode
     unless @search.latitude.present? && @search.longitude.present?
-      if request.location && request.location.country != 'Reserved' && request.location.state
-        coords = Geocoder.coordinates("#{@search.location}, #{request.location.state}, #{request.location.country}")
-      elsif request.location && request.location.country != 'Reserved'
+      if request.location && request.location.country != 'Reserved'
         coords = Geocoder.coordinates("#{@search.location}, #{request.location.country}")
       else
         coords = Geocoder.coordinates(@search.location)
