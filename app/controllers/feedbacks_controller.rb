a0e6590fac4bedde77165f8d26e7ec39f9fce590
Sebastian Edwards <s.thursday@gmail.com>
2012-09-17 15:55:41 +1200

    Removing old rating code.

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -8,14 +8,6 @@ class FeedbacksController < ApplicationController
     else
       render :show
     end
-    # if current_user
-    #   klass = env['PATH_INFO'].split('/').second.singularize.capitalize
-    #   @rating = Rating.find_or_create_by_user_id_and_ratable_type_and_ratable_id(current_user.id, klass, params[:homestay_id])
-    #   @rating.update_attributes(params[:rating])
-    #   render json: @rating.to_json
-    # else
-    #   render nothing: true, status: 401
-    # end
   end
 
   def show
