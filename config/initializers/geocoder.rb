f59d6ae3950654724f255476abda077047143d8f
Sebastian Edwards <s.thursday@gmail.com>
2012-10-25 12:52:20 +1300

    Updating geocoder.

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,6 +1,6 @@
 Geocoder.configure do |config|
   config.lookup = :bing
   config.api_key = 'AvPoJ5-fGVx4lv7m9JaDGmiX213KBlrAh373TEk_5epnjPgK79vGD1o58voxGIJw'
-  config.timeout = 6
+  config.timeout = 10
   config.units = :km
 end
