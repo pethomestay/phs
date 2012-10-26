ae37a43f37e03671ecb73c9ffde2c09f5a7d86ff
Sebastian Edwards <s.thursday@gmail.com>
2012-10-26 16:21:02 +1300

    Collect pet date of birth rather than age.

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -33,7 +33,7 @@ class Pet < ActiveRecord::Base
                   :other_pet_type, :emergency_contact_name, :emergency_contact_phone, :vet_name, \
                   :vet_phone, :medication
 
-  validates_presence_of :name, :age, :emergency_contact_name, :emergency_contact_phone
+  validates_presence_of :name, :date_of_birth, :emergency_contact_name, :emergency_contact_phone
   validates_inclusion_of :pet_type, :in => %w( dog cat bird fish other )
   validates_inclusion_of :size, :in => %w( small medium large giant ), if: Proc.new {|pet| pet.pet_type == 'dog'}
   validates_inclusion_of :sex, :in => %w( male_desexed female_desexed male_entire female_entire ), if: Proc.new {|pet| ['cat', 'dog'].include?(pet.pet_type)}
@@ -48,6 +48,17 @@ class Pet < ActiveRecord::Base
     dislikes << 'Dislikes other animals' if dislike_animals
 
     dislikes.join(', ')
+  end
+
+  def age
+    now = Time.now.utc.to_date
+    years = now.year - date_of_birth.year - ((now.month > date_of_birth.month || (now.month == date_of_birth.month && now.day >= date_of_birth.day)) ? 0 : 1)
+
+    if years > 0
+      "#{years} #{'years'.pluralize(years)} old"
+    else
+      "Less than 1 year old"
+    end
   end
 
   def pretty_pet_type
