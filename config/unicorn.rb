8d5ca0fe83d024b49c6c2d6513627ffca9f848ab
Sebastian Edwards <s.thursday@gmail.com>
2012-11-09 10:46:49 +1300

    Use only one unicorn in development.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,7 @@
-worker_processes 4 # amount of unicorn workers to spin up
+if ENV['RAILS_ENV'] == 'production'
+  worker_processes 4
+else
+  worker_processes 1
+end
 timeout 30         # restarts workers that hang for 30 seconds
 preload_app true
