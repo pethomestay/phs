2a6dc93149aa03c942d6333435845d6d4b40b955
Sebastian Edwards <s.thursday@gmail.com>
2012-11-13 13:27:51 +1300

    Really fix user destroy.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,7 +6,7 @@ class RegistrationsController < Devise::RegistrationsController
     if current_user.homestay.present?
       current_user.homestay.update_attribute :active, false
     end
-    current_user.update_attribute :email, "#{current_user.email}.old"
+    current_user.update_attribute :email, "#{current_user.email}.#{Time.now.to_i}.old"
     sign_out(current_user)
     redirect_to root_path, alert: "Thanks for trying PetHomeStay."
   end
