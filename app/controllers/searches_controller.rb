8eb7e8a470e5cc882570d3d1f3ee827ff50998ea
Sebastian Edwards <sebastianedwards@Sebastians-MacBook-Pro.local>
2013-01-06 19:30:59 +1300

    Ensure only active homestays are shown to pet owners.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -29,7 +29,7 @@ class SearchesController < ApplicationController
       @homestays = Homestay.active.near([@search.latitude, @search.longitude], @search.within, order: @search.sort_by)
                            .paginate(page: params[:page], per_page: 10)
     else
-      ids = Homestay.near([@search.latitude, @search.longitude], @search.within, order: 'distance').pluck(:id)
+      ids = Homestay.active.near([@search.latitude, @search.longitude], @search.within, order: 'distance').pluck(:id)
       homestays_with_feedbacks = Homestay.active.where(id: ids).includes(user: :received_feedbacks)
       @homestays = homestays_with_feedbacks.sort_by!{|h| h.average_rating}.reverse.paginate(page: params[:page], per_page: 10)
     end
@@ -45,7 +45,7 @@ class SearchesController < ApplicationController
       @homestays = Homestay.active.near([@search.latitude, @search.longitude], @search.within, order: @search.sort_by)
                            .paginate(page: params[:page], per_page: 10)
     else
-      ids = Homestay.near([@search.latitude, @search.longitude], @search.within, order: 'distance').pluck(:id)
+      ids = Homestay.active.near([@search.latitude, @search.longitude], @search.within, order: 'distance').pluck(:id)
       homestays_with_feedbacks = Homestay.active.where(id: ids).includes(user: :received_feedbacks)
       @homestays = homestays_with_feedbacks.sort_by!{|h| h.average_rating}.reverse.paginate(page: params[:page], per_page: 10)
     end
