81af6ddf94e772c5e6089eed4a538605875eebc6
Adam Boas <adam.boas@gmail.com>
2013-02-09 22:09:13 +1100

    removed Raven initializer and fixed Geocoder initializer to use a hash rather than a block

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,6 +1,6 @@
-Geocoder.configure do |config|
-  config.lookup = :bing
-  config.api_key = 'AvPoJ5-fGVx4lv7m9JaDGmiX213KBlrAh373TEk_5epnjPgK79vGD1o58voxGIJw'
-  config.timeout = 10
-  config.units = :km
+Geocoder.configure(
+  lookup: :bing,
+  api_key: 'AvPoJ5-fGVx4lv7m9JaDGmiX213KBlrAh373TEk_5epnjPgK79vGD1o58voxGIJw',
+  timeout: 10,
+  units: :km
 end
