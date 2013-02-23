6be36363c27432d4c4cf58429b1542c512be4190
Adam Boas <adam.boas@gmail.com>
2013-02-24 09:17:27 +1100

    Added a spec for the rating star helper

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -18,4 +18,28 @@ describe ApplicationHelper do
       end
     end
   end
+
+  describe '#rating_stars' do
+    before do
+      helper.extend Haml
+      helper.extend Haml::Helpers 
+      helper.send :init_haml_helpers
+    end
+
+    it 'should output a number of i elements with the class icon-star equal to the rating' do
+      (1..5).each do |rating|
+        helper.capture_haml do
+          helper.rating_stars rating 
+        end.should have_selector('i', class: 'icon-star', count: rating)
+      end
+    end
+
+    it 'should output a number of i elements with the class icon-star-empty equal to 5 minus the rating' do
+      (1..5).each do |rating|
+        helper.capture_haml do
+          helper.rating_stars rating 
+        end.should have_selector('i', class: 'icon-star-empty', count: (5 - rating))
+      end
+    end
+  end
 end
