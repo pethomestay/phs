7ec374371f10f3b2d41c94938cb8b7d44d3e8143
Adam Boas <adam.boas@gmail.com>
2013-03-25 11:00:48 +1100

    removed searches controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -20,14 +20,9 @@ PetHomestay::Application.routes.draw do
     resources :users
   end
 
-  get '/searches'       => 'searches#create'
-  get '/pet-care/:city' => 'searches#show', as: 'city_search'
+  get '/my-account'     => 'users#show', as: :my_account
+  get '/my-account'     => 'users#show', as: :user_root
 
-  get '/my-account'     => 'users#show', as: 'my_account'
-  get '/my-account'     => 'users#show', as: 'user_root'
-  root to: "pages#home"
-
-  get '/searches/none'        => 'pages#home', as: 'no_results'
   get '/how-does-it-work'     => 'pages#how_does_it_work', as: 'how_does_it_work'
   get '/what-is'              => 'pages#about_us', as: 'what_is'
   get '/why-join-pethomestay' => 'pages#why_join_pethomestay', as: 'why_join'
@@ -36,4 +31,5 @@ PetHomestay::Application.routes.draw do
   get '/house-rules'          => 'pages#house_rules', as: 'house_rules'
   get '/privacy-policy'       => 'pages#privacy_policy', as: 'privacy_policy'
   get '/faqs'                 => 'pages#faqs', as: 'faqs'
+  root to: "pages#home"
 end
