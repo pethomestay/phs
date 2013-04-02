52a992bfa641bdb152ca00641d66b1c2d5648a5a
Adam Boas <adam.boas@gmail.com>
2013-04-02 22:08:55 +1100

    Added more details and a links to Enquiry members

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,4 +1,4 @@
 Time::DATE_FORMATS[:day_and_month] = "%d %B"
-Time::DATE_FORMATS[:day_month_year] = "%Y-%m-%d"
+Time::DATE_FORMATS[:year_month_day] = "%Y-%m-%d"
 Date::DATE_FORMATS[:day_and_month] = "%d %B"
-Date::DATE_FORMATS[:day_month_year] = "%Y-%m-%d"
+Date::DATE_FORMATS[:year_month_day] = "%Y-%m-%d"
