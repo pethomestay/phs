f42eab0ad3798171cc1e6611951a364dc2ade01e
Adam Boas <adam.boas@gmail.com>
2013-05-12 08:12:27 +1000

    move back to the defaults for geocoding (bing seems unreliable on my localhost)

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,6 +1,10 @@
 Geocoder.configure(
-  lookup: :bing,
-  api_key: 'AvPoJ5-fGVx4lv7m9JaDGmiX213KBlrAh373TEk_5epnjPgK79vGD1o58voxGIJw',
-  timeout: 20,
   units: :km
 )
+
+# Geocoder.configure(
+#   lookup: :bing,
+#   api_key: 'AvPoJ5-fGVx4lv7m9JaDGmiX213KBlrAh373TEk_5epnjPgK79vGD1o58voxGIJw',
+#   timeout: 20,
+#   units: :km
+# )
