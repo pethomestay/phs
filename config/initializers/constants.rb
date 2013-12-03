5d5eb3b89449ec39640ddcef066ed49fdb96f24a
bilalbash <bilal.basharat@gmail.com>
2013-12-04 02:28:12 +0500

    fixed envirnment variable

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,12 +1,12 @@
 SYSTEM_FEE = 10
 if Rails.env == "development"
-	MERCHANT_ID = "EHY0047"
-	TRANSACTION_PASSWORD = "zggiztda"
-	TRANSACTION_RESULT_URL = "bookings/transaction_result?authenticity_token="
+	ENV['MERCHANT_ID']= "EHY0047"
+	ENV['TRANSACTION_PASSWORD'] = "zggiztda"
+	ENV['TRANSACTION_RESULT_URL'] = "bookings/transaction_result?authenticity_token="
 else
-	MERCHANT_ID = "EHY0047"
-	TRANSACTION_PASSWORD = "zggiztda"
-	REDIRECT_URL = "bookings/transaction_result?authenticity_token="
-	# MERCHANT_ID = "EHY0047" # live credentials
-	# TRANSACTION_PASSWORD = "wyj85zzc" # live credentials
+	ENV['MERCHANT_ID'] = "EHY0047"
+	ENV['TRANSACTION_PASSWORD'] = "zggiztda"
+	ENV['TRANSACTION_RESULT_URL'] = "bookings/transaction_result?authenticity_token="
+	# ENV['MERCHANT_ID'] = "EHY0047" # live credentials
+	# ENV['TRANSACTION_PASSWORD'] = "wyj85zzc" # live credentials
 end
