993da71c8d8167589c45eeab50ed60e5f98a5302
bilalbash <bilal.basharat@gmail.com>
2013-12-11 10:29:14 +0500

    heroku mailer settings without mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,20 +65,30 @@ PetHomestay::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  # Use mailgun for ActionMailer
-  ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'www.pethomsetay.com',
-    :authentication => :plain,
-  }
-  ActionMailer::Base.delivery_method = :smtp
+  # TODO: uncomment the following mailer settings for production server
+  ## Use mailgun for ActionMailer
+  #ActionMailer::Base.smtp_settings = {
+  #  :port           => ENV['MAILGUN_SMTP_PORT'],
+  #  :address        => ENV['MAILGUN_SMTP_SERVER'],
+  #  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+  #  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+  #  :domain         => 'www.pethomsetay.com',
+  #  :authentication => :plain,
+  #}
+  #ActionMailer::Base.delivery_method = :smtp
+  #
+  ## Add default mailer URL
+  #config.action_mailer.default_url_options = { :host => 'www.pethomestay.com' }
+  #config.action_mailer.asset_host = "http://www.pethomestay.com"
+
+  # TODO: following action_mailer settings are temporary for heroku staging server
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = false
 
   # Add default mailer URL
-  config.action_mailer.default_url_options = { :host => 'www.pethomestay.com' }
-  config.action_mailer.asset_host = "http://www.pethomestay.com"
+  config.action_mailer.default_url_options = { :host => 'pethomestay.herokuapp.com' }
+  config.action_mailer.asset_host = 'http://pethomestay.herokuapp.com'
+  config.action_mailer.delivery_method = :test
 
   # Use SSL
   # config.force_ssl = true
