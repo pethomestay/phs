ca9f6d400be79dd15e7c5206a1c08d0cc95caff3
Daryl Moulder <dmoulder@qtome.com>
2014-01-17 18:26:51 +1100

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 PetHomestay::Application.routes.draw do
 
-  devise_for :users, controllers: {registrations: "registrations"}
+  devise_for :users, controllers: {registrations: "registrations",  :omniauth_callbacks => "users/omniauth_callbacks"}
 
   resources :contacts, only: [:new, :create]
   resources :enquiries, only: [:create, :show, :update] do
