0a1a279a053bd7984b56112ef4092ecea0b02429
Daryl Moulder <dmoulder@qtome.com>
2014-03-24 17:13:15 +1100

    fix error in no homestay or new homestay in nav

--- app/controllers/availability_controller.rb
+++ app/controllers/availability_controller.rb
@@ -4,9 +4,6 @@ class AvailabilityController < ApplicationController
 
   def show
     @homestay = Homestay.find_by_slug(params[:id])
-    test = 1
-
-    #@booking.remove_notification if @booking.host_accepted? && @booking.owner_view?(current_user)
   end
 end
 
