11e7dd3831c4744df2562bc8cf62b511570956b6
Daryl Moulder <dmoulder@qtome.com>
2014-04-30 13:57:26 +1000

    shorten cancel info

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -26,9 +26,9 @@ module BookingsHelper
       if booking.status == HOST_HAS_REQUESTED_CANCELLATION
         return 'Host requested cancellation'
       elsif booking.status == BOOKING_STATUS_HOST_CANCELED
-          return 'Host cancelled this booking'
+          return 'Host cancelled'
       else
-          return 'Guest cancelled this booking'
+          return 'Guest cancelled'
       end
     elsif (booking.host_accepted)
       return  'Confirmed'
