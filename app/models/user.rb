76d0f6061c3530222a1820a3df17a9c5abec2d4a
Daryl Moulder <dmoulder@qtome.com>
2014-05-13 13:02:21 +1000

    rename sanatise to sanitise and the env var too for spelling fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -228,7 +228,7 @@ class User < ActiveRecord::Base
 		"#{self.address_1} #{self.address_suburb}, #{self.address_city}, #{self.address_country}."
   end
 
-  def sanatise
+  def sanitise
     clean_email = self.email
     if clean_email.include? "<" #in the form of Joe Blogs <joe.blogs@company.com>
       start_str = "<"
