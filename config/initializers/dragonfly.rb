d16b33c57d74a025bcea96b545ad54205ee499aa
James Drucza <james@drucza.net>
2014-05-27 23:48:59 +1000

    Forgot the host url shenanigans

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -9,7 +9,8 @@ if Rails.env.staging? || Rails.env.production?
       :bucket_name => ENV['S3_BUCKET'],
       :access_key_id => ENV['S3_KEY'],
       :secret_access_key => ENV['S3_SECRET'],
-      :region => ENV['S3_REGION']
+      :region => ENV['S3_REGION'],
+      :url_host => ENV['S3_URL_HOST']
     )
   end
 end
