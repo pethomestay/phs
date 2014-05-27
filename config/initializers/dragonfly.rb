d673a0a9f027195f6f3fa72d5cdf9e1415c0f9fc
James Drucza <james@drucza.net>
2014-05-27 23:33:58 +1000

    Adding s3 region to dragonfly settings

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -8,7 +8,8 @@ if Rails.env.staging? || Rails.env.production?
     c.datastore = Dragonfly::DataStorage::S3DataStore.new(
       :bucket_name => ENV['S3_BUCKET'],
       :access_key_id => ENV['S3_KEY'],
-      :secret_access_key => ENV['S3_SECRET']
+      :secret_access_key => ENV['S3_SECRET'],
+      :region => ENV['S3_REGION']
     )
   end
 end
