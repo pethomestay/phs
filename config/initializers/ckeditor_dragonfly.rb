d906507658e4f19cd9dad8e80dd86858ad1205cc
Daryl Moulder <dmoulder@qtome.com>
2014-05-30 14:43:59 +1000

    add config for s3 S3_IAM_PROFILE

--- config/initializers/ckeditor_dragonfly.rb
+++ config/initializers/ckeditor_dragonfly.rb
@@ -25,7 +25,8 @@ app.configure do
               access_key_id: ENV['S3_KEY'],
               secret_access_key: ENV['S3_SECRET'],
               region:  ENV['S3_REGION'],
-              url_host: ENV['S3_URL_HOST']
+              url_host: ENV['S3_URL_HOST'],
+              use_iam_profile: ENV['S3_IAM_PROFILE']
   else
     datastore :file,
               root_path:  Rails.root.join("public", "uploads", "ckeditor", Rails.env).to_s,
