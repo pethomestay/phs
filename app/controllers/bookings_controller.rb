bd5fa8d1101a2f3b97ccd60e678d69a4805741b2
abhimanyu <abhimanyu.singh2086@gmail.com>
2014-06-25 16:55:31 +0530

    fixed bug in validating host's availability

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -73,7 +73,7 @@ class BookingsController < ApplicationController
 
 	def update_transaction
 		booking = Booking.find(params[:booking_id])
-    unavailable_dates = booking.bookee.unavailable_dates_between(params[:check_in_date], params[:check_out_date])
+    unavailable_dates = booking.bookee.unavailable_dates_between(params[:check_in_date].to_date, params[:check_out_date].to_date)
     if unavailable_dates.blank?  
       transaction_payload = booking.update_transaction_by(params[:number_of_nights], params[:check_in_date], params[:check_out_date])
       return render json: transaction_payload
@@ -86,7 +86,7 @@ class BookingsController < ApplicationController
 		booking = Booking.find(params[:booking_id])
 		booking.check_in_time = params[:check_in_time]
 		booking.check_out_time = params[:check_out_time]
-    unavailable_dates = booking.bookee.unavailable_dates_between(params[:check_in_date], params[:check_out_date])
+    unavailable_dates = booking.bookee.unavailable_dates_between(params[:check_in_date].to_date, params[:check_out_date].to_date)
     if unavailable_dates.blank?
       booking.message_update(params[:message])
       render nothing: true
