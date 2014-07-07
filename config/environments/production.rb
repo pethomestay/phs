4018fc83e4b5179b1d20d79d7be0b3290233755a
Xuebing Li <xuebinglee@gmail.com>
2014-07-07 14:56:35 +1000

    Correct scss/css filename

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ PetHomestay::Application.configure do
   # config.assets.precompile += %w( search.js )
   config.assets.precompile += %w( ckeditor/plugins/youtube/plugin.js )
   config.assets.precompile += %w( ckeditor/plugins/youtube/lang/en.js )
-  config.assets.precompile += %w( new_home.scss )
+  config.assets.precompile += %w( new_home.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
