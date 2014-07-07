81f262a97ddaff2ed33b26f59030e6331f6a0e1a
Xuebing Li <xuebinglee@gmail.com>
2014-07-07 14:48:42 +1000

    Rename new_home.scss

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -46,7 +46,7 @@ PetHomestay::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( new_home.css.scss )
+  config.assets.precompile += %w( new_home.scss )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
