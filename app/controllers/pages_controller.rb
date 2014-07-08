f2905631b6499c712796c99c36bd8649fd28b047
Xuebing Li <xuebinglee@gmail.com>
2014-07-08 14:48:03 +1000

    Remove useless password protection

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   before_filter :authenticate_user!, only: [:welcome]
-  before_filter :authenticate
 
   def home
 		@homepage = true
@@ -32,16 +31,5 @@ class PagesController < ApplicationController
   end
 
   def post_to_securepay
-  end
-
-  protected
-  def authenticate
-    if  ENV['GOOGLE_ANALYTICS_TRACKING_ID'] == "UA-32892865-1" #we are in a prod env
-      return true
-    else
-      authenticate_or_request_with_http_basic do | username, password |
-        username == ENV['HTTP_USERNAME'] and password == ENV['HTTP_PASSWORD']
-      end
-    end
   end
 end
