6ee909c2621195d0b4c35101bd3919edde3b2e12
Xuebing Li <xuebinglee@gmail.com>
2014-09-16 14:36:48 +1000

    Verify existence of homestay for host view

--- app/controllers/host/host_controller.rb
+++ app/controllers/host/host_controller.rb
@@ -2,6 +2,7 @@ class Host::HostController < ApplicationController
   layout 'new_application'
 
   before_filter :authenticate_user!
+  before_filter :verify_homestay_existance!
   before_filter :host_view
 
   # GET /host
@@ -11,5 +12,9 @@ class Host::HostController < ApplicationController
 
   def host_view
     @host_view = true
+  end
+
+  def verify_homestay_existance!
+    redirect_to new_homestay_path if current_user.homestay.blank?
   end
 end
