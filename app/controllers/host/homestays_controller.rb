e75c623eba2cc5530379da8a2842d9f0c91b536b
Xuebing Li <xuebinglee@gmail.com>
2014-10-02 15:38:58 +1000

    Refactor Homestay#edit

--- app/controllers/host/homestays_controller.rb
+++ app/controllers/host/homestays_controller.rb
@@ -8,13 +8,12 @@ class Host::HomestaysController < Host::HostController
 
   # GET /host/homestays/edit
   def edit
-    @my_homestay = current_user.homestay
-    raise ActiveRecord::RecordNotFound unless @my_homestay
-    if not @my_homestay.active?
-      notice = 'This listing is not active.'
-    elsif @my_homestay.locked?
-      notice = 'This listing is locked.'
+    @homestay = current_user.homestay
+    raise ActiveRecord::RecordNotFound unless @homestay
+    if not @homestay.active?
+      flash[:alert] = 'This listing is not active.'
+    elsif @homestay.locked?
+      flash[:alert] = 'This listing is locked.'
     end
-    flash.now[:notice] = notice if notice
   end
 end
