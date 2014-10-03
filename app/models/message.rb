244e8f674be4ce7437ee9f36dd47a0e1c01c83d6
Xuebing Li <xuebinglee@gmail.com>
2014-10-03 16:27:26 +1000

    Mark conversation unread after message creation

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,7 @@ class Message < ActiveRecord::Base
 	belongs_to :user
 
 	after_create :enquiry_response_message
+	after_create :mark_mailbox_unread
 
 	def enquiry_response_message
 		if mailbox.booking.blank? && (mailbox.messages.size <= 2) && (user == mailbox.host_mailbox) && (mailbox.messages.first.user == mailbox.guest_mailbox)
@@ -12,5 +13,16 @@ class Message < ActiveRecord::Base
 
 	def to_user
 		(mailbox.guest_mailbox == user) ? mailbox.host_mailbox : mailbox.guest_mailbox
+	end
+
+	private
+	def mark_mailbox_unread
+		mailbox = self.mailbox
+		if self.user == mailbox.guest_mailbox
+			mailbox.host_read  = false
+		elsif self.user == mailbox.host_mailbox
+			mailbox.guest_read = false
+		end
+		mailbox.save
 	end
 end
