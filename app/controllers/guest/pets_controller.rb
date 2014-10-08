564b129f0916feeeea6f6a3197db4b48b4e652eb
Xuebing Li <xuebinglee@gmail.com>
2014-10-08 11:44:06 +1100

    Add index page to guest pets

--- app/controllers/guest/pets_controller.rb
+++ app/controllers/guest/pets_controller.rb
@@ -1,10 +1,16 @@
 class Guest::PetsController < Guest::GuestController
+  respond_to :html
+
   # GET /guest/pets
   def index
+    @pets = current_user.pets
+    respond_with @pets
   end
 
   # GET /guest/pets/new
   def new
-    @pet = Pet.new
+    @pet = current_user.pets.build
+    respond_with @pet
+  end
   end
 end
