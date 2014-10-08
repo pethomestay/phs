87e045e4793725bb5f8bb36aa3051318b2e2497d
Xuebing Li <xuebinglee@gmail.com>
2014-10-09 10:56:19 +1100

    Set instance variable for update action as well

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
   before_filter :authenticate_user!
-  before_filter :set_instance_vars, only: [:edit]
+  before_filter :set_instance_vars, only: [:edit, :update]
 
   def edit
     super
