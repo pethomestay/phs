530ab530c478fa92e8fd93a19502b73825c07052
Xuebing Li <xuebinglee@gmail.com>
2014-10-10 14:04:30 +1100

    Install jQuery file upload plugin

--- app/assets/javascripts/new_application.js
+++ app/assets/javascripts/new_application.js
@@ -12,6 +12,11 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.ui.widget
+//= require jquery.iframe-transport
+//= require jquery.fileupload
+//= require cloudinary/jquery.cloudinary
+//= require attachinary
 //= require flight-for-rails
 //= require bootstrap.min
 //= require bootstrap-growl.min
