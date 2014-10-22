c0a3e1be4bc59692408595ce0f97efc5781adac0
Xuebing Li <xuebinglee@gmail.com>
2014-10-22 15:40:36 +1100

    Work in progress: Guest's past bookings

--- app/controllers/guest/favorites_controller.rb
+++ app/controllers/guest/favorites_controller.rb
@@ -2,5 +2,6 @@ class Guest::FavoritesController < Guest::GuestController
   # GET /guest/favorites
   def index
     @favorites = current_user.homestays
+    @hosts_with_bookings = current_user.bookers.booked.group_by { |b| b.bookee }
   end
 end
