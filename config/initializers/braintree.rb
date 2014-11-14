b6869fe4da7a94ff0d3196a0a9f10f0838ff4abc
Xuebing Li <xuebinglee@gmail.com>
2014-11-14 14:29:28 +1100

    Use env variables to configure Braintree

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,11 +1,8 @@
-if Rails.env == "development"
-  Braintree::Configuration.environment = :sandbox
-  Braintree::Configuration.merchant_id = "nfss7wx3fk4dzkrg"
-  Braintree::Configuration.public_key = "9mbnqmrx5js2dvm7"
-  Braintree::Configuration.private_key = "043a7de25e089ba5befc4e74ae91dad3"
-else
-  Braintree::Configuration.environment = :production
-  Braintree::Configuration.merchant_id = 'h2cb3xyy7sv7xrfp'
-  Braintree::Configuration.public_key = 'qzn64tyg6jmxvnpx'
-  Braintree::Configuration.private_key = 'e43d290912e89f58a32cda916af1100b'
-end
+Braintree::Configuration.environment =
+  ENV['BRAINTREE_PRIVATE_KEY'] ? :production : :sandbox
+Braintree::Configuration.merchant_id =
+  ENV['BRAINTREE_MERCHANT_ID'] || 'nfss7wx3fk4dzkrg'
+Braintree::Configuration.public_key  =
+  ENV['BRAINTREE_PUBLIC_KEY']  || '9mbnqmrx5js2dvm7'
+Braintree::Configuration.private_key =
+  ENV['BRAINTREE_PRIVATE_KEY'] || '043a7de25e089ba5befc4e74ae91dad3'
