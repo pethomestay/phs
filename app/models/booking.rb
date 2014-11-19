3bcf036a61ba2322e50d2c501c18e3ce19b6e658
albertjou <albert_jou@hotmail.com>
2014-11-19 17:19:19 +1100

    Admin reconciliation screen
    Only shows paid bookings that have been accepted & .csv fix

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -119,7 +119,7 @@ class Booking < ActiveRecord::Base
       csv << [
         'Guest name', 'Guest address', 'Pet name', 'Pet breed', 'Pet age', 'Check-in Date', 'Check-in Time',
         'Check-out Date', 'Check-out Time', 'Host name', 'Homestay Title', 'Host Address', '# of 24 hour period',
-        'Transaction Reference', 'Total', 'Insurance Fees', 'PHS Fee', 'Host Payout', 'Coupon' 'Status'
+        'Transaction Reference', 'Total', 'Insurance Fees', 'PHS Fee', 'Host Payout','Status'
       ]
 
       all.each do |booking|
@@ -128,14 +128,13 @@ class Booking < ActiveRecord::Base
         host = booking.bookee
         booking_reference = booking.transaction.present? ? booking.transaction.reference.to_s : booking.payment.braintree_transaction_id
         booking_payment_amount = booking.transaction.present? ? "$#{booking.transaction.amount}" : "$#{booking.payment.amount.to_s}"
-        booking_coupon = booking.coupon.present? ? "#{booking.coupon.code} ($#{booking.coupon.discount_amount.to_s} off)" : "none"
         csv << [
           booker.name.capitalize, booker.complete_address, pet.name, pet.breed, pet.age,
           booking.check_in_date.to_formatted_s(:year_month_day), booking.check_in_time.strftime("%H:%M"),
           booking.check_out_date.to_formatted_s(:year_month_day), booking.check_out_time.strftime("%H:%M"),
           host.name.capitalize, booking.homestay.nil? ? "" : booking.homestay.title, host.complete_address, booking.number_of_nights,
           booking_reference, booking_payment_amount, "$#{booking.public_liability_insurance}",
-          "$#{booking.phs_service_charge}", "$#{booking.host_payout}", booking_coupon,
+          "$#{booking.phs_service_charge}", "$#{booking.host_payout}",
           (booking.state?(:host_paid)) ? 'Paid' : 'Not Paid'
         ]
       end
