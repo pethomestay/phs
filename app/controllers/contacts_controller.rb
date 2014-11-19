1227ec6c7adaf638ec029adde5dffeff3abf0403
Xuebing Li <xuebinglee@gmail.com>
2014-11-19 17:55:11 +1100

    Make contact us form responsive

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,17 +2,17 @@ class ContactsController < ApplicationController
   respond_to :html
 
   def new
-    respond_with @contact = Contact.new
+    respond_with @contact = Contact.new, layout: 'new_application'
   end
 
   def create
     @contact = Contact.new(params[:contact])
     if @contact.valid?
       @contact.send_email
-      flash[:notice] = 'Your message has been sent'
+      flash[:notice] = 'Your message has been sent. We usually respond within an hour or so during AEST business hours!'
       redirect_to :back
     else
-      render :new
+      render :new, layout: 'new_application'
     end
   end
 end
