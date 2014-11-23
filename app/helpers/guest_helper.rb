86eeb0a2b38d46a308c4e37a499573566570941f
Xuebing Li <xuebinglee@gmail.com>
2014-11-24 10:02:55 +1100

    Refactor to move date check into view

--- app/helpers/guest_helper.rb
+++ app/helpers/guest_helper.rb
@@ -1,7 +1,7 @@
 module GuestHelper
   def set_instance_vars
     @unread_count = Mailbox.as_guest(current_user).where(guest_read: false).count
-    @awaits_feedback = current_user.bookers.sort_by(&:check_in_date).select {|b| b.check_out_date < Date.today && b.try(:enquiry).try(:feedbacks).try(:empty?) && view_context.translate_state(b.state) == 'Booked' }
+    @awaits_feedback = current_user.bookers.sort_by(&:check_in_date).select {|b| b.try(:enquiry).try(:feedbacks).try(:empty?) && view_context.translate_state(b.state) == 'Booked' }
     @upcoming = current_user.bookers.sort_by(&:check_in_date).select {|b| b.check_out_date >= Date.today}
   end
 end
