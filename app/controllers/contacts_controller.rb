682d7ce063ad450b6b8047bc38e7131b8c8c01b2
Xuebing Li <xuebinglee@gmail.com>
2014-11-24 15:17:02 +1100

    Send contact requests directly to intercom

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,9 +8,27 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
     if @contact.valid?
-      @contact.send_email
+      if user_signed_in?
+        Intercom::Message.create({
+          from: {
+            type: 'user',
+            user_id: current_user.id,
+          },
+          body: params[:contact][:message]
+        })
+      else
+        user = User.find_by_email(params[:contact][:email])
+        user = Intercom::User.create(email: params[:contact][:email], name: params[:contact][:name]) unless user.present?
+        Intercom::Message.create({
+          from: {
+            type: 'user',
+            email: user.email,
+          },
+          body: params[:contact][:message]
+        })
+      end
       flash[:notice] = 'Your message has been sent. We usually respond within an hour or so during AEST business hours!'
-      redirect_to :back
+      redirect_to new_contact_path
     else
       render :new, layout: 'new_application'
     end
