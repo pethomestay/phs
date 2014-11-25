b8184a973d6552a8ddcb9cdf8cabec02481c6971
Xuebing Li <xuebinglee@gmail.com>
2014-11-25 17:37:10 +1100

    Make the tem page responsive

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,41 +1,34 @@
 class PagesController < ApplicationController
+  layout 'new_application'
   before_filter :authenticate_user!, only: [:welcome]
 
   def home
     @check_for_coupon = params[:check_for_coupon].present? && current_user.try(:admin)
     @homepage = true
-    render layout: 'new_application'
   end
 
   def welcome
-    render layout: 'new_application'
   end
 
   def why_join_pethomestay
-    render layout: 'new_application'
   end
 
   def our_company
-    render layout: 'new_application'
   end
 
   def the_team
   end
 
   def investors
-    render layout: 'new_application'
   end
 
   def jobs
-    render layout: 'new_application'
   end
 
   def partners
     @contact = Contact.new
-    render layout: 'new_application'
   end
 
   def in_the_press
-    render layout: 'new_application'
   end
 end
