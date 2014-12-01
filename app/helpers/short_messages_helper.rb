059a39994b557a20acdaf1b6391c61dba07013be
Xuebing Li <xuebinglee@gmail.com>
2014-12-01 17:51:22 +1100

    Don't send sms if user opts out sms

--- app/helpers/short_messages_helper.rb
+++ app/helpers/short_messages_helper.rb
@@ -5,13 +5,15 @@ module ShortMessagesHelper
 
   def send_sms(opts)
     user = opts[:to]
-    params = {
-      username: ENV['SMSBROADCAST_USERNAME'],
-      password: ENV['SMSBROADCAST_PASSWORD'],
-      from: 'PetHomeStay',
-      to: user.mobile_number,
-      message: opts[:text]
-    }
-    RestClient.get SMSBROADCAST_URL, params: params
+    unless user.opt_out_sms
+      params = {
+        username: ENV['SMSBROADCAST_USERNAME'],
+        password: ENV['SMSBROADCAST_PASSWORD'],
+        from: 'PetHomeStay',
+        to: user.mobile_number,
+        message: opts[:text]
+      }
+      RestClient.get SMSBROADCAST_URL, params: params
+    end
   end
 end
