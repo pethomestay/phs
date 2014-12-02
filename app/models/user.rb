d2bc3e4c45f673711db3a9d3af5f467491cdf24a
Xuebing Li <xuebinglee@gmail.com>
2014-12-02 17:59:45 +1100

    Convert indentation into spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -99,11 +99,11 @@ class User < ActiveRecord::Base
   end
 
   def booking_accepted_by_host?
-	  booking_accepted_by_host.any?
+    booking_accepted_by_host.any?
   end
 
   def booking_accepted_by_host
-		self.bookers.accepted_by_host
+    self.bookers.accepted_by_host
   end
 
   def validate_code?(code)
@@ -116,19 +116,19 @@ class User < ActiveRecord::Base
   end
 
   def booking_declined_by_host?
-	  booking_declined_by_host.any?
+    booking_declined_by_host.any?
   end
 
   def booking_declined_by_host
-	  self.bookers.declined_by_host
+    self.bookers.declined_by_host
   end
 
   def booking_required_response?
-	  booking_required_response.any?
+    booking_required_response.any?
   end
 
   def booking_required_response
-	  self.bookers.required_response
+    self.bookers.required_response
   end
 
   def unanswered_enquiries?
@@ -144,11 +144,11 @@ class User < ActiveRecord::Base
   end
 
   def booking_needing_confirmation?
-	  booking_needing_confirmation.any?
+    booking_needing_confirmation.any?
   end
 
   def booking_needing_confirmation
-	  homestay.blank? ? [] : homestay.bookings.needing_host_confirmation
+    homestay.blank? ? [] : homestay.bookings.needing_host_confirmation
   end
 
   def enquiries_needing_confirmation
@@ -186,11 +186,11 @@ class User < ActiveRecord::Base
   end
 
   def pet_breed
-		pets.map(&:breed).to_sentence
+    pets.map(&:breed).to_sentence
   end
 
   def pet
-	  self.pets.first unless self.pets.blank?
+    self.pets.first unless self.pets.blank?
   end
 
   def update_average_rating
@@ -201,62 +201,62 @@ class User < ActiveRecord::Base
   def find_or_create_booking_by(enquiry=nil, homestay=nil)
     homestay = enquiry.homestay if homestay.nil?
     booking = enquiry.booking || Booking.new
-	  # unfinished_bookings = self.bookers.unfinished.where(:homestay_id=>homestay_id).all()
-	  # booking = unfinished_bookings.blank? ? self.bookers.build : unfinished_bookings.first
+    # unfinished_bookings = self.bookers.unfinished.where(:homestay_id=>homestay_id).all()
+    # booking = unfinished_bookings.blank? ? self.bookers.build : unfinished_bookings.first
 
-		booking.enquiry = enquiry
-	  booking.homestay = homestay
-	  booking.bookee = homestay.user
-	  booking.cost_per_night = homestay.cost_per_night
+    booking.enquiry = enquiry
+    booking.homestay = homestay
+    booking.bookee = homestay.user
+    booking.cost_per_night = homestay.cost_per_night
     booking.booker = enquiry.user
 
-	  date_time_now = DateTime.now
-	  time_now = Time.now
+    date_time_now = DateTime.now
+    time_now = Time.now
     if booking.check_in_date.blank? or booking.check_in_time.blank? or booking.check_out_time.blank? or booking.check_out_date.blank? #set the date/time  if not already set
-	    booking.check_in_date = enquiry.blank? ? date_time_now : (enquiry.check_in_date.blank? ? date_time_now : enquiry.check_in_date)
-	    booking.check_in_time = enquiry.blank? ? time_now : (enquiry.check_in_time.blank? ? time_now : enquiry.check_in_time)
-	    booking.check_out_date = enquiry.blank? ? date_time_now : (enquiry.check_out_date.blank? ? date_time_now : enquiry.check_out_date)
-	    booking.check_out_time = enquiry.blank? ? time_now : (enquiry.check_out_time.blank? ? time_now : enquiry.check_out_time)
+      booking.check_in_date = enquiry.blank? ? date_time_now : (enquiry.check_in_date.blank? ? date_time_now : enquiry.check_in_date)
+      booking.check_in_time = enquiry.blank? ? time_now : (enquiry.check_in_time.blank? ? time_now : enquiry.check_in_time)
+      booking.check_out_date = enquiry.blank? ? date_time_now : (enquiry.check_out_date.blank? ? date_time_now : enquiry.check_out_date)
+      booking.check_out_time = enquiry.blank? ? time_now : (enquiry.check_out_time.blank? ? time_now : enquiry.check_out_time)
     end
-	  number_of_nights = (booking.check_out_date - booking.check_in_date).to_i
-		booking.number_of_nights = number_of_nights <= 0 ? 1 : number_of_nights
+    number_of_nights = (booking.check_out_date - booking.check_in_date).to_i
+    booking.number_of_nights = number_of_nights <= 0 ? 1 : number_of_nights
 
-	  booking.subtotal = booking.cost_per_night * booking.number_of_nights
-	  booking.amount = booking.calculate_amount
+    booking.subtotal = booking.cost_per_night * booking.number_of_nights
+    booking.amount = booking.calculate_amount
     booking.host_accepted = nil
     booking.owner_accepted = nil
-	  booking.save!
+    booking.save!
     booking.mailbox.update_attribute(:booking_id, booking.id)
-	  booking
+    booking
   end
 
-	def find_or_create_transaction_by(booking)
-		transaction = booking.transaction.blank? ? Transaction.find_or_create_by_booking_id(booking.id) : booking.transaction
+  def find_or_create_transaction_by(booking)
+    transaction = booking.transaction.blank? ? Transaction.find_or_create_by_booking_id(booking.id) : booking.transaction
 
-		transaction.reference = "transaction_id=#{transaction.id}"
-		transaction.type_code = 1 # preauth type is 1, simple transaction type is 0
-		transaction.amount = booking.amount
-		transaction.time_stamp = Time.now.gmtime.strftime("%Y%m%d%H%M%S")
+    transaction.reference = "transaction_id=#{transaction.id}"
+    transaction.type_code = 1 # preauth type is 1, simple transaction type is 0
+    transaction.amount = booking.amount
+    transaction.time_stamp = Time.now.gmtime.strftime("%Y%m%d%H%M%S")
 
-		fingerprint_string = "#{ENV['MERCHANT_ID']}|#{ENV['TRANSACTION_PASSWORD']}|#{transaction.type_code}|#{transaction.
-				reference}|#{transaction.actual_amount}|#{transaction.time_stamp}"
+    fingerprint_string = "#{ENV['MERCHANT_ID']}|#{ENV['TRANSACTION_PASSWORD']}|#{transaction.type_code}|#{transaction.
+        reference}|#{transaction.actual_amount}|#{transaction.time_stamp}"
 
-		transaction.merchant_fingerprint = Digest::SHA1.hexdigest(fingerprint_string)
+    transaction.merchant_fingerprint = Digest::SHA1.hexdigest(fingerprint_string)
 
-		transaction.save!
-		transaction
-	end
+    transaction.save!
+    transaction
+  end
 
   def find_stored_card_id(selected_stored_card=nil, use_stored_card=nil)
-		if selected_stored_card.blank?
-			if use_stored_card.blank?
-			  return nil
-			elsif use_stored_card.to_s == '1' && self.cards.size >= 1
-				return self.cards.first.id
-			end
-		else
-			return selected_stored_card
-		end
+    if selected_stored_card.blank?
+      if use_stored_card.blank?
+        return nil
+      elsif use_stored_card.to_s == '1' && self.cards.size >= 1
+        return self.cards.first.id
+      end
+    else
+      return selected_stored_card
+    end
   end
 
   def unlink_from_facebook
@@ -283,7 +283,7 @@ class User < ActiveRecord::Base
   end
 
   def complete_address
-		"#{self.address_1} #{self.address_suburb}, #{self.address_city}, #{self.address_country}."
+    "#{self.address_1} #{self.address_suburb}, #{self.address_city}, #{self.address_country}."
   end
 
   def sanitise
