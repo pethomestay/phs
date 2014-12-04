b27c4f3e185694d717c43048aaa9bfc0725b38fc
albertjou <albert_jou@hotmail.com>
2014-12-04 14:05:47 +1100

    **STAGING ONLY**

--- app/models/coupon_usage.rb
+++ app/models/coupon_usage.rb
@@ -4,4 +4,11 @@ class CouponUsage < ActiveRecord::Base
   belongs_to :booking
   attr_accessible :coupon_status, :user_id, :coupon_id
   scope :unused, -> { where("booking_id IS NULL")}
+  after_create :increment_counter
+
+  private
+
+  def increment_counter
+    self.coupon.increment(:users_count)
+  end
 end
