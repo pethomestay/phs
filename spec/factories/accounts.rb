6b37850a8da91ff63b8083a948acfc816b048fb5
Xuebing Li <xuebinglee@gmail.com>
2014-12-11 14:18:03 +1100

    Convert indentation to spaces

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
 
-	factory :account do
-		user { FactoryGirl.create :user }
-		account_number '1234567'
-		bsb '1233456'
-		name 'Bilal Basharat'
-	end
+  factory :account do
+    user { FactoryGirl.create :user }
+    account_number '1234567'
+    bsb '1233456'
+    name 'Bilal Basharat'
+  end
 end
