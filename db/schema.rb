59be15055a6d9fbc4ed6e2c27aa075a8b360868f
Xuebing Li <xuebinglee@gmail.com>
2014-12-11 15:51:22 +1100

    Remove bank name from database

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,11 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141211041900) do
+ActiveRecord::Schema.define(:version => 20141211044623) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "user_id"
     t.string   "name"
-    t.string   "bank"
     t.string   "bsb"
     t.string   "account_number"
     t.datetime "created_at",     :null => false
