758e74382f411ff19ad92f1bedb4b56823850cbc
Xuebing Li <xuebinglee@gmail.com>
2014-12-16 16:30:26 +1100

    Make all_personalities a constent

---
+++ config/initializers/personalities.rb
@@ -0,0 +1,6 @@
+PERSONALITIES = [
+  'Barker', 'Digger', 'Chewer', 'Playful', 'Energetic', 'Mellow', 'Smart',
+  'Obedient', 'Mischievous', 'Loud', 'Quiet', 'Protective', 'Affectionate',
+  'Crazy', 'Independent', 'Rowdy', 'Calm', 'Athletic', 'Lively', 'Happy',
+  'Gentle'
+]
