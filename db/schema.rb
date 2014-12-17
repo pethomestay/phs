cce0a8c3d914fdf1ae8e4af30cde9e8380f4d0ed
Xuebing Li <xuebinglee@gmail.com>
2014-12-17 17:22:08 +1100

    Drop table tags & taggings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141216235017) do
+ActiveRecord::Schema.define(:version => 20141217053133) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "user_id"
@@ -281,24 +281,6 @@ ActiveRecord::Schema.define(:version => 20141216235017) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
   end
-
-  create_table "taggings", :force => true do |t|
-    t.integer  "tag_id"
-    t.integer  "taggable_id"
-    t.string   "taggable_type"
-    t.integer  "tagger_id"
-    t.string   "tagger_type"
-    t.string   "context",       :limit => 128
-    t.datetime "created_at"
-  end
-
-  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], :name => "taggings_idx", :unique => true
-
-  create_table "tags", :force => true do |t|
-    t.string "name"
-  end
-
-  add_index "tags", ["name"], :name => "index_tags_on_name", :unique => true
 
   create_table "transactions", :force => true do |t|
     t.integer  "booking_id"
