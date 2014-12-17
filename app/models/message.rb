02cdb52025edd072fe3ba3accaba03d66ff9aa6f
Xuebing Li <xuebinglee@gmail.com>
2014-12-17 14:39:45 +1100

    Comment out legacy code

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,20 +2,21 @@ class Message < ActiveRecord::Base
   belongs_to :mailbox
   belongs_to :user
 
-  after_create :enquiry_response_message
+  # after_create :enquiry_response_message
   after_create :mark_mailbox_unread
 
-  def enquiry_response_message
-    if mailbox.booking.blank? && (mailbox.messages.size <= 2) && (user == mailbox.host_mailbox) && (mailbox.messages.first.user == mailbox.guest_mailbox)
-      PetOwnerMailer.host_enquiry_response(mailbox.enquiry).deliver
-    end
-  end
+  # def enquiry_response_message
+  #   if mailbox.booking.blank? && (mailbox.messages.size <= 2) && (user == mailbox.host_mailbox) && (mailbox.messages.first.user == mailbox.guest_mailbox)
+  #     PetOwnerMailer.host_enquiry_response(mailbox.enquiry).deliver
+  #   end
+  # end
 
   def to_user
     (mailbox.guest_mailbox == user) ? mailbox.host_mailbox : mailbox.guest_mailbox
   end
 
   private
+
   def mark_mailbox_unread
     mailbox = self.mailbox
     if self.user == mailbox.guest_mailbox
