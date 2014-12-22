52669e5fa76e712e2b5a4fa81cfa76684a892e92
Xuebing Li <xuebinglee@gmail.com>
2014-12-22 12:40:21 +1100

    Create reference data: supervison

---
+++ app/models/reference_data/supervision.rb
@@ -0,0 +1,26 @@
+class ReferenceData::Supervision
+  attr_reader :id, :title
+
+  @@all = []
+
+  def initialize(opts={})
+    @id    = opts[:id]
+    @title = opts[:title]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find(id)
+    @@all.each do |supervision|
+      return supervision.title if supervision.id == id
+    end
+    raise ActiveRecord::RecordNotFound.new
+  end
+end
+
+ANYTIME       = ReferenceData::Supervision.new id: '1', title: '24x7 Care'
+EVENINGS      = ReferenceData::Supervision.new id: '2', title: 'Evenings'
+WEEKENDS_ONLY = ReferenceData::Supervision.new id: '3', title: 'Weekends Only'
