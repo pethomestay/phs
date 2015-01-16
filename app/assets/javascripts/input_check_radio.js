7bbbcd81d5aa6ffa6760dff8510175e2a5254cf3
Hashim <zehash@gmail.com>
2015-01-16 11:08:49 +1100

    New Radio Button logic implemented - Radio button styles set across host form and pet profile - General UI fixes

--- app/assets/javascripts/input_check_radio.js
+++ app/assets/javascripts/input_check_radio.js
@@ -1,6 +1,27 @@
 $(document).ready(function() {
 
+  checkRadioValues();
   checkCheckBoxValues();
+
+
+  function checkRadioValues() {
+    $('.collection_rb').each(function(){
+      if( $(this).prop( "checked" ) == true ){
+           $(this).addClass( "active" );
+      } else {
+         $(this).removeClass( "active" );
+      }
+    });
+
+    $('.boolean_rb').each(function(){
+      if($(this).prop( "checked" ) == true){
+           $(this).addClass( "active" );
+      } else {
+         $(this).removeClass( "active" );
+      }
+    });
+
+  }
 
   function checkCheckBoxValues() {
     $('.collection_cb').each(function(){
@@ -20,6 +41,20 @@ $(document).ready(function() {
     });
 
   }
+    
+    $( ".collection_rb" ).click(function() { 
+
+      if( !$(this).hasClass("active") ) {
+          $( this ).parent().parent().children().children().removeClass( "active" );
+          $( this ).parent().parent().children().children().attr( "checked", false );
+          console.log($(this).parent().parent());
+          $(this).addClass( "active" );
+          $(this).attr("checked", true);
+        }  
+
+    }); 
+
+
     $( ".collection_cb" ).click(function() { 
       if( $(this).hasClass("active") ) {
           $(this).removeClass( "active" );
