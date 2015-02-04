7c7472bd8fa6ee096b28854df1f2f819c6562872
albertjou <albert_jou@hotmail.com>
2015-02-05 09:44:19 +1100

    Bug fix: duplicate bookin card in right panel

--- app/helpers/guest_helper.rb
+++ app/helpers/guest_helper.rb
@@ -2,6 +2,6 @@ module GuestHelper
   def set_instance_vars
     @unread_count = Mailbox.as_guest(current_user).where(guest_read: false).count
     @awaits_feedback = current_user.bookers.sort_by(&:check_in_date).select {|b| b.try(:enquiry).try(:feedbacks).try(:empty?) && view_context.translate_state(b.state) == 'Booked' }
-    @upcoming = current_user.bookers.sort_by(&:check_in_date).select {|b| b.check_out_date >= Date.today}
+    @upcoming = current_user.bookers.sort_by(&:check_in_date).select {|b| b.check_out_date >= Date.today} - @awaits_feedback
   end
 end
