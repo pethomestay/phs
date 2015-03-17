5c23cafeca85c233242915ebaa9d5ab4bc89ac06
Hashim <zehash@gmail.com>
2015-03-17 17:28:45 +1100

    Fixed the round function for average rating

--- app/models/user.rb
+++ app/models/user.rb
@@ -226,8 +226,7 @@ class User < ActiveRecord::Base
   end
 
   def update_average_rating
-    rating = received_feedbacks.count == 0 ? 0 : (received_feedbacks.sum('rating') / received_feedbacks.count).to_f
-    rating.round
+    rating = received_feedbacks.count == 0 ? 0 : (received_feedbacks.sum('rating').to_f / received_feedbacks.count.to_f).round
     update_attribute :average_rating, rating
   end
 
