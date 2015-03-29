ef374875333e2835f8e9fd17c263601b3568d499
Hashim <zehash@gmail.com>
2015-03-30 07:23:17 +1100

    Testing social sharing

--- app/assets/javascripts/wizard_setup.js
+++ app/assets/javascripts/wizard_setup.js
@@ -214,42 +214,6 @@ $(document).ready(function() {
       isValid = false;
    });
 
-   // $('.fb-share-btn.fb-test i').on('click', function() {
-   //    console.debug($(this).data());
-   // });
-
-   window.fbAsyncInit = function() {
-    FB.init({
-      appId      : "#{ENV['APP_ID'] || '382802968555135' }",
-      cookie     : true,
-      status     : true,
-      xfbml      : true,
-      version    : 'v2.1'
-    });
-
-    $('.fb-share-btn.fb-guest-to-host-review').on('click', function() {
-      var data = $(this).data();
-      console.debug(data);
-      var fb_title = "Check out my review of " + data.title + "'s homestay!" 
-      FB.ui(
-      {
-        method: 'feed',
-        link: "https://www.pethomestay.com.au",
-        name: fb_title,
-        description: data.review
-      }, function(response){});
-    });
-
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
 });
 
 
