19ef7234bf373519d856a5e7d3344788bb84dce7
Hashim <zehash@gmail.com>
2015-08-06 10:46:28 +1000

    Edit host profile page to read easier.
    https://www.pivotaltracker.com/n/projects/1108894/stories/100634472
    
    Started the refactor of this page with new features such as a new gallery section and removal of redundant code.
    
    #Fixes feature <100634472>

---
+++ app/assets/javascripts/homestays/show.js
@@ -0,0 +1,98 @@
+  $(document).scroll(function () {
+      //stick nav to top of page
+      var y = $(this).scrollTop();
+      var navWrap = $('#navWrap').offset().top;
+      if (y > navWrap) {
+          $('nav').addClass('bar_top_sticky');
+      } else {
+          $('nav').removeClass('bar_top_sticky');
+      }
+  });
+ 
+  Galleria.run('.galleria', {
+    transition: 'fade',
+    imageCrop: true
+  });
+  $(function () {
+      $('[data-toggle="tooltip"]').tooltip()
+  });
+
+  var $check_in, $check_out, $map, check_in_picker, check_out_picker, map;
+  if (window.location.hash === '#request-modal-add-pet') {
+    $('#request-modal-add-pet').modal('show');
+  }
+  if (window.location.hash === '#request-modal') {
+    $('#request-modal').modal('show');
+  }
+  if (window.location.hash === '#confirm-modal') {
+    $('#confirm-modal').modal('show');
+  }
+
+  $map = $('#map');
+  map = new GMaps({
+      div: '#map',
+      lat: $map.data('lat'),
+      lng: $map.data('lng'),
+      minZoom: 10,
+      maxZoom: 14,
+      disableDefaultUI: true,
+      scrollwheel: false,
+      draggable: false
+  });
+  map.addMarker({
+      lat: $map.data('lat'),
+      lng: $map.data('lng'),
+      icon: 'https://res.cloudinary.com/hxnfgf9c2/image/upload/v1414374978/map_marker.png'
+   });
+  $('#map').mouseenter(function(evt) {
+    if (!map.hover) {
+      map.hover = true;
+      return map.setOptions({
+        panControl: true,
+        zoomControl: true,
+        zoomControlOptions: {
+          style: google.maps.ZoomControlStyle.LARGE
+        }
+      });
+    }
+  });
+  $('body').mouseover(function(evt) {
+    if (map.hover) {
+      if ($(evt.target).closest('#map').length === 0) {
+        map.hover = false;
+        return map.setOptions({
+          panControl: false,
+          zoomControl: false
+        });
+      }
+    }
+  });
+  $check_in = $('#enquiry_check_in_date');
+  check_in_picker = $check_in.datepicker({
+    format: 'dd/mm/yyyy',
+    startDate: '0d',
+    autoclose: true
+  }).on('changeDate', function(evt) {
+    var inDate;
+    inDate = new Date(evt.date);
+    check_out_picker.setDate(inDate);
+    check_out_picker.startDate = inDate;
+    check_out_picker.update();
+    return $check_out.focus();
+  }).data('datepicker');
+  $check_out = $('#enquiry_check_out_date');
+  check_out_picker = $check_out.datepicker({
+    format: 'dd/mm/yyyy',
+    startDate: '0d',
+    autoclose: true
+  }).data('datepicker');
+  $('.enquiry_check_in_date .input-group-addon').on('click', function(evt) {
+    evt.preventDefault();
+    check_out_picker.hide();
+    return check_in_picker.show();
+  });
+  $('.enquiry_check_out_date .input-group-addon').on('click', function(evt) {
+    evt.preventDefault();
+    check_in_picker.hide();
+    return check_out_picker.show();
+  });
