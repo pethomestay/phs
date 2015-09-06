eb635539b3572262b8004411331244f8b82302d8
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2015-09-06 21:43:13 +0800

    Moved Pet methods to UserDecorator

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,3 +1,23 @@
 class UserDecorator < SimpleDelegator
 
+  def pet_name
+    if pets.length == 1
+      pets.first.name
+    else
+      "your pets"
+    end
+  end
+
+  def pet_names
+    pets.map(&:name).to_sentence
+  end
+
+  def pet_breed
+    pets.map(&:breed).to_sentence
+  end
+
+  def pet
+    pets.first
+  end
+
 end
--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -14,19 +14,36 @@ RSpec.describe UserDecorator do
     end
 
     context 'without pet' do
-      it "returns 'your pets'"
+      it "returns 'your pets'" do
+        expect(decorator.pet_name).to eq 'your pets'
+      end
     end
   end
 
   describe '#pet_names' do
-    it 'returns pet names in sentence'
+    let!(:pet1) { create :pet, user: user }
+    let!(:pet2) { create :pet, user: user }
+
+    it 'returns pet names in sentence' do
+      expect(decorator.pet_names).to eq "#{pet2.name} and #{pet1.name}"
+    end
   end
 
   describe '#pet_breed' do
-    it 'returns pet breed in sentence'
+    let!(:pet1) { create :pet, user: user }
+    let!(:pet2) { create :pet, user: user }
+
+    it 'returns pet breed in sentence' do
+      expect(decorator.pet_breed).to eq "#{pet2.breed} and #{pet1.breed}"
+    end
+
   end
 
   describe '#pet' do
-    it 'returns first pet'
+    let!(:pet) { create :pet, user: user }
+
+    it 'returns first pet' do
+      expect(decorator.pet).to eq pet
+    end
   end
 end
