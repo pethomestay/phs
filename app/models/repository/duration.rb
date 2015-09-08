0109cad657ddf9a411eda510be333cd7ac84dc9a
Robbie Marcelo <rbmrclo@hotmail.com>
2015-09-09 00:19:53 +0800

    Adds duration repository model

---
+++ app/models/repository/duration.rb
@@ -0,0 +1,8 @@
+class Repository::Duration < ActiveYaml::Base
+
+  set_filename "durations"
+
+  field :title
+  field :natural
+
+end
---
+++ spec/models/repository/duration_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+RSpec.describe Repository::Duration do
+
+  subject { Repository::Duration }
+
+  describe ".all" do
+    it {
+      expected_titles = [
+        "Morning",
+        "Afternoon",
+        "Evening",
+        "Overnight",
+        "2 nights",
+        "3 nights",
+        "4 nights",
+        "5 nights",
+        "6 nights",
+        "7 nights",
+        "Longer"
+      ]
+
+      expect(subject.all.map(&:title)).to match_array expected_titles
+    }
+
+    it {
+      expected_naturals = [
+        "the morning",
+        "the afternoon",
+        "the evening",
+        "an overnight visit",
+        "2 nights",
+        "3 nights",
+        "4 nights",
+        "5 nights",
+        "6 nights",
+        "7 nights",
+        "Longer than 7 nights"
+      ]
+
+      expect(subject.all.map(&:natural)).to match_array expected_naturals
+    }
+  end
+
+end
