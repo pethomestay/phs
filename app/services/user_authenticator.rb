8187ef7dc57af9da467c206159580e9f947cb12c
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2015-09-12 14:50:09 +0800

    Add Yard for UserAuthenticator

--- app/services/user_authenticator.rb
+++ app/services/user_authenticator.rb
@@ -3,11 +3,22 @@ class UserAuthenticator
   # Extend / Subclass this class when other authentications are needed
   attr_accessor :current_user, :auth
 
+  # Create an instance of UserAuthenticator
+  #
+  # @params
+  #   current_user [User]
+  #   auth [Hash]
+  # @api public
+  # @return [UserAuthenticator]
   def initialize(current_user, auth)
     @current_user = current_user
     @auth = auth
   end
 
+  # Authenticate User
+  #
+  # @api public
+  # @return [User]
   def authenticate
     if user.new_record? #must be a new user fill in the details
       set_user_details
@@ -20,28 +31,53 @@ class UserAuthenticator
     user
   end
 
+  # Show User
+  #
+  # @api public
+  # @return [User]
   def user
     @user ||= current_user || provider_user || email_user || new_user
   end
 
   private
 
+  # Retreive graph object
+  #
+  # @api private
+  # @return [Koala::Facebook::API]
   def graph
     @graph ||= Koala::Facebook::API.new(auth.credentials.token)
   end
   
+  # Return graph object for user
+  #
+  # @api private
+  # @return [Koala::Facebook::API]
   def me
     @me ||= graph.get_object("me")
   end
 
+  # Return graph permissions for user
+  #
+  # @api private
+  # @return [Koala::Facebook::API]
   def permissions
     @permissions ||= graph.get_connections('me','permissions')
   end
 
+
+  # Return graph age for user
+  #
+  # @api private
+  # @return [Koala::Facebook::API]
   def age_info
     @age_info ||= graph.get_object("me", :fields=>"age_range")
   end
 
+  # Set user details
+  #
+  # @api private
+  # @return [Hash]
   def set_user_details
     user.attributes = {
       email:      me["email"] ,
@@ -51,6 +87,10 @@ class UserAuthenticator
     }
   end
 
+  # Set user location
+  #
+  # @api private
+  # @return [Hash]
   def set_user_location
     location_info =  me["location"]
     if location_info
@@ -58,6 +98,10 @@ class UserAuthenticator
     end
   end
 
+  # Set user age
+  #
+  # @api private
+  # @return [Hash]
   def set_user_age
     user.attributes = {
       age_range_min: age_info['age_range']['min'],
@@ -65,20 +109,36 @@ class UserAuthenticator
     }
   end
 
+  # Persist user
+  #
+  # @api private
+  # @return [User]
   def persist_user
     user.skip_confirmation! # don't need to confirm if this is a Facebook user
     user.update_attributes(provider: auth.provider, uid: auth.uid)
     user.save
   end
 
+  # Find user from provider
+  #
+  # @api private
+  # @return [User]
   def provider_user
     User.where(provider: auth.provider, uid: auth.uid).first 
   end
 
+  # Find user from email
+  #
+  # @api private
+  # @return [User]
   def email_user
     User.where(email: me["email"]).first 
   end
 
+  # New user
+  #
+  # @api private
+  # @return [User]
   def new_user
     User.new(email: me["email"], provider: auth.provider, uid: auth.uid)
   end
