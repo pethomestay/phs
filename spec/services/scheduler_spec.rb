49de15617012fb9e49cc40060ef98a0c311673f9
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2015-09-12 21:43:57 +0800

    Add Initial Scheduler

---
+++ spec/services/scheduler_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Scheduler do
+  describe '#initialize' do
+    it 'sets schedulable'
+  end
+
+  describe '#booking_info_between'
+  describe '#unavailable_dates_info'
+  describe '#unavailable_dates_after'
+  describe '#unavailable_dates_between'
+end
