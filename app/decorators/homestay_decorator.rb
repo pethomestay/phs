0f28cec9efbc7b752ac2b6ca2c586b3a559d809a
Robbie Marcelo <rbmrclo@hotmail.com>
2015-09-14 00:51:53 +0800

    Adds homestay decorator + extract methods from homestay model

---
+++ app/decorators/homestay_decorator.rb
@@ -0,0 +1,64 @@
+class HomestayDecorator < SimpleDelegator
+
+  def geocoding_address
+    if address_suburb.nil? && address_1.nil?
+      "#{address_city}, #{address_country}"
+    elsif address_suburb.nil?
+      "#{address_1}, #{address_city}, #{address_country}"
+    elsif address_1.nil?
+      "#{address_suburb}, #{address_city}, #{address_country}"
+    elsif address_suburb.present? && address_1.present?
+      "#{address_1}, #{address_suburb}, #{address_city}, #{address_country}"
+    end
+  end
+
+  def auto_decline_sms_text
+    auto_decline_sms.present? ? auto_decline_sms : auto_decline_sms_template
+  end
+
+  def auto_interest_sms_text
+    auto_interest_sms.present? ? auto_interest_sms : auto_interest_sms_template
+  end
+
+  def pretty_emergency_preparedness
+    if first_aid? && emergency_transport?
+      "I know pet first-aid and can provide emergency transport"
+    elsif first_aid?
+      "I know pet first-aid"
+    elsif emergency_transport?
+      "I can provide emergency transport"
+    end
+  end
+
+  # Depreciated but may be used in other parts of the program, especially emails
+  def pretty_supervision
+    if constant_supervision
+      "I can provide 24/7 supervision for your pets"
+    elsif supervision_outside_work_hours
+      "I can provide supervision for your pets outside work hours (8am - 6pm)"
+    end
+  end
+
+  def location
+    if address_suburb != address_city
+      "#{address_suburb}, #{address_city}" # "Avoid Melbourne, Melbourne"
+    else
+      "#{address_suburb}"
+    end
+  end
+
+  def user_contact
+    user.mobile_number.present? ? user.mobile_number : user.email
+  end
+
+  private
+
+    def auto_interest_sms_template
+      "Hi, I would love to help look after your pet. Let's arrange a time to meet. My contact is #{user_contact}"
+    end
+
+    def auto_decline_sms_template
+      "Sorry - I can't help this time, but please ask again in the future!"
+    end
+
+end
---
+++ spec/decorators/homestay_decorator_spec.rb
@@ -0,0 +1,202 @@
+require 'rails_helper'
+
+RSpec.describe HomestayDecorator do
+  let!(:homestay)  { create :homestay }
+  let!(:decorator) { HomestayDecorator.new(homestay) }
+
+  describe "#geocoding_address" do
+    context "address_suburb is nil" do
+      before { homestay.address_suburb = nil }
+
+      it "returns address_1, address_city, and address_country" do
+        expected = "#{homestay.address_1}, #{homestay.address_city}, #{homestay.address_country}"
+
+        expect(decorator.geocoding_address).to eq expected
+      end
+    end
+
+    context "address_1 is nil" do
+      before { homestay.address_1 = nil }
+
+      it "returns address_suburb, address_city, and address_country" do
+        expected = "#{homestay.address_suburb}, #{homestay.address_city}, #{homestay.address_country}"
+
+        expect(decorator.geocoding_address).to eq expected
+      end
+    end
+
+    context "address_suburb and address_1 is present" do
+      before do
+        homestay.address_1 = "test address_1"
+        homestay.address_suburb = "test suburb"
+      end
+
+      it "returns address_1, address_suburb, address_city, and address_country" do
+        expected = "#{homestay.address_1}, #{homestay.address_suburb}, #{homestay.address_city}, #{homestay.address_country}"
+        expect(decorator.geocoding_address).to eq expected
+      end
+    end
+
+    context "address_suburb and address_1 is nil" do
+      before do
+        homestay.address_1 = nil
+        homestay.address_suburb = nil
+      end
+
+      it "returns address_city and address_country" do
+        expected = "#{homestay.address_city}, #{homestay.address_country}"
+
+        expect(decorator.geocoding_address).to eq expected
+      end
+    end
+  end
+
+  describe "#pretty_emergency_preparedness" do
+    context "first_aid and emergency_transport are true" do
+      before do
+        homestay.first_aid = true
+        homestay.emergency_transport = true
+      end
+
+      it { expect(decorator.pretty_emergency_preparedness).to eq "I know pet first-aid and can provide emergency transport" }
+    end
+
+    context "first_aid is true" do
+      before do
+        homestay.first_aid = true
+        homestay.emergency_transport = false
+      end
+
+      it { expect(decorator.pretty_emergency_preparedness).to eq "I know pet first-aid" }
+    end
+
+    context "emergency_transport is true" do
+      before do
+        homestay.first_aid = false
+        homestay.emergency_transport = true
+      end
+
+      it { expect(decorator.pretty_emergency_preparedness).to eq "I can provide emergency transport" }
+    end
+  end
+
+  describe "#auto_decline_sms_text" do
+    context "auto_decline_sms is present" do
+      before { homestay.auto_decline_sms = "test auto_decline_sms" }
+
+      it { expect(decorator.auto_decline_sms_text).to eq homestay.auto_decline_sms }
+    end
+
+    context "auto_decline_sms is nil" do
+      before { homestay.auto_decline_sms = nil }
+
+      it { expect(decorator.auto_decline_sms_text).to eq "Sorry - I can't help this time, but please ask again in the future!" }
+    end
+
+    context "auto_decline_sms is empty string" do
+      before { homestay.auto_decline_sms = "" }
+
+      it { expect(decorator.auto_decline_sms_text).to eq "Sorry - I can't help this time, but please ask again in the future!" }
+    end
+  end
+
+  describe "#user_contact" do
+    let!(:user) { homestay.user }
+
+    context "user mobile is present" do
+      before { homestay.user.mobile_number = "09270000000" }
+
+      it "returns user mobile" do
+        expect(decorator.user_contact).to eq user.mobile_number
+      end
+    end
+
+    context "user mobile is blank" do
+      before do
+        homestay.user.mobile_number = nil
+        homestay.user.email = "sample@pethomestay.com"
+      end
+
+      it "returns user email" do
+        expect(decorator.user_contact).to eq user.email
+      end
+    end
+  end
+
+  describe "#auto_interest_sms_text" do
+    context "auto_interest_sms is present" do
+      before { homestay.auto_interest_sms = "test auto_interest_sms" }
+
+      it { expect(decorator.auto_interest_sms_text).to eq homestay.auto_interest_sms }
+    end
+
+    shared_examples "returns auto_interest_sms_template with user_contact" do
+      context "user mobile number is present" do
+        before do
+          homestay.user.mobile_number = "09277000000"
+        end
+
+        it { expect(decorator.auto_interest_sms_text).to eq "Hi, I would love to help look after your pet. Let's arrange a time to meet. My contact is 09277000000" }
+      end
+
+      context "user mobile is blank" do
+        before do
+          homestay.user.mobile_number = nil
+          homestay.user.email = "sample@pethomestay.com"
+        end
+
+        it { expect(decorator.auto_interest_sms_text).to eq "Hi, I would love to help look after your pet. Let's arrange a time to meet. My contact is sample@pethomestay.com" }
+      end
+    end
+
+    context "auto_interest_sms is nil" do
+      before { homestay.auto_interest_sms = nil }
+
+      it_behaves_like "returns auto_interest_sms_template with user_contact"
+    end
+
+    context "auto_interest_sms is empty string" do
+      before { homestay.auto_interest_sms = "" }
+
+      it_behaves_like "returns auto_interest_sms_template with user_contact"
+    end
+  end
+
+  describe "#location" do
+    context "address_suburb is not the same as address_city" do
+      before do
+        homestay.address_suburb = "test suburb"
+        homestay.address_city = "test city"
+      end
+
+      it { expect(decorator.location).to eq "#{homestay.address_suburb}, #{homestay.address_city}" }
+    end
+
+    context "address_suburb is the same as address_city" do
+      before do
+        homestay.address_suburb = "test suburb"
+        homestay.address_city = "test suburb"
+      end
+
+      it { expect(decorator.location).to eq "#{homestay.address_suburb}" }
+    end
+  end
+
+  describe "#pretty_supervision" do
+    context "constant_supervision is true" do
+      before { homestay.constant_supervision = true }
+
+      it { expect(decorator.pretty_supervision).to eq "I can provide 24/7 supervision for your pets" }
+    end
+
+    context "constant_supervision is true" do
+      before do
+        homestay.constant_supervision = false
+        homestay.supervision_outside_work_hours = true
+      end
+
+      it { expect(decorator.pretty_supervision).to eq "I can provide supervision for your pets outside work hours (8am - 6pm)" }
+    end
+  end
+
+end
