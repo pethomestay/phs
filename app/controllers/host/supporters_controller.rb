6c518525bb6bf4b2dfc31552ea181e592436c723
Hashim <zehash@gmail.com>
2015-09-28 16:33:29 +1000

    WireFrames for supporters
    https://www.pivotaltracker.com/n/projects/1108894/stories/96159164
    
    At this time users can pull out their gmail contacts, and then filter them by whichever one they want. Next Im going to replug that to the sending emails function. The tab "Get Supporters" text has been modified as per the ticket. UI changed as well.
    
    hotfix/HR-96159164
    
    Still in development

--- app/controllers/host/supporters_controller.rb
+++ app/controllers/host/supporters_controller.rb
@@ -3,14 +3,35 @@ respond_to :html
 
   def index
     @recommendations = current_user.recommendations.order('created_at DESC').paginate(page: params[:page], per_page: 10)
+    puts "Im being called again and again"
+    @emails = Rails.cache.fetch("emails") 
   end
 
-
   def invite_emails
-    @contacts = request.env['omnicontacts.contacts']
-    @contacts.each do |contact|
-      UserMailer.invite_email(contact[:email], current_user).deliver
-    end
+    @contacts ||= request.env['omnicontacts.contacts']
+    @emails = @contacts.map { |contact| contact[:emails] [0][:email] if contact[:emails] } 
+    puts @emails
+    # listbox sect2
+
+    Rails.cache.write("emails", @emails) 
+    # @contacts.each do |contact|
+      # UserMailer.invite_email(contact[:email], current_user).deliver
+    # end
     redirect_to host_supporters_path, :notice => "Invitaion mail sent successfully"  
   end  
+
+
+
+  # Route for sending supporter invite emails after users have filtered through them.
+  # This is an Ajax call that should return a 200 response if successful
+  # or some 503 response if not successful.
+  # The view is to add the logic of what to show the host and when.
+  def send_invite_emails
+
+
+
+
+  end  
+
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,8 @@ PetHomestay::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.action_controller.perform_caching = true
+
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
