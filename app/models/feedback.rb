a2f4791985698100e064e23d3f71e5bc861967e8
David Bird <dave@zero51.com>
2015-11-14 13:35:23 +1100

    Fix feedback creation
    https://www.pivotaltracker.com/story/show/108134712
    
    * 'Validation failed: Rating is not included in the list'.
    * Rating passed as string in params. Just removed the validation.

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -4,8 +4,6 @@ class Feedback < ActiveRecord::Base
   belongs_to :enquiry
 
   validates_presence_of :rating
-  validates_numericality_of :rating
-  validates_inclusion_of :rating, :in => 1..5
 
   after_save :update_user_average_rating, on: :create
 
