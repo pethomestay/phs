035aba96646c5a5cfdeabc3bc00c4b7669672889
David Bird <dave@zero51.com>
2015-11-24 18:36:45 +1100

    Search page tweaks

--- app/assets/javascripts/components_ui/nabo.js
+++ app/assets/javascripts/components_ui/nabo.js
@@ -3,9 +3,4 @@ $(document).ready(function() {
     e.preventDefault();
     window.open($(this).attr('href'));
   });
-
-  $('.referer').on('click', function(e) {
-    var a = $(this).find('a');
-    window.open(a.attr('href'));
-  });
 });
