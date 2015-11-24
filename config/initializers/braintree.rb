3522438606c32dac958ef3a567d77f27786489a2
David Bird <dave@zero51.com>
2015-11-24 13:13:56 +1100

    Pull Braintree config from ENV

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,11 +1,4 @@
-if Rails.env.production?
-  Braintree::Configuration.environment = :production
-  Braintree::Configuration.merchant_id = 'h2cb3xyy7sv7xrfp'
-  Braintree::Configuration.public_key = 'qzn64tyg6jmxvnpx'
-  Braintree::Configuration.private_key = 'e43d290912e89f58a32cda916af1100b'
-else
-  Braintree::Configuration.environment = :sandbox
-  Braintree::Configuration.merchant_id = ENV['BRAINTREE_MERCHANT_ID']
-  Braintree::Configuration.public_key = ENV['BRAINTREE_PUBLIC_KEY']
-  Braintree::Configuration.private_key = ENV['BRAINTREE_PRIVATE_KEY']
-end
+Braintree::Configuration.environment = :sandbox
+Braintree::Configuration.merchant_id = ENV['BRAINTREE_MERCHANT_ID']
+Braintree::Configuration.public_key = ENV['BRAINTREE_PUBLIC_KEY']
+Braintree::Configuration.private_key = ENV['BRAINTREE_PRIVATE_KEY']
